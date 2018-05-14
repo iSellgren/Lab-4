99fd8e0023db1e3367df2b8c67ae5acd30f1f9c7
iSellgren <frse1700@student.miun.se>
2018-05-14 20:36:40 +0200

    Har mött sista bossen....

--- Lab 4/main.cpp
+++ Lab 4/main.cpp
@@ -43,7 +43,8 @@ struct labyrinth {
     }
     
 
-    #define SIZE 19
+    #define SIZE 14
+    
     std::vector <std::vector<Block>> maze;
 
 };
@@ -66,6 +67,8 @@ void Drill(std::stack<std::pair<int,int>>&back_track,std::stack<std::pair<int,in
 
         level.maze[back_track.top().first][back_track.top().second].display = Block::PATH;
         level.maze[back_track.top().first][back_track.top().second].visited = true;
+        level.maze[back_track.top().first][back_track.top().second].display = Block::PATH;
+        level.maze[back_track.top().first][back_track.top().second].visited = true;
         level.maze[back_track.top().first][back_track.top().second].bot_wall = false;
         
         back_track.push(std::make_pair(back_track.top().first,back_track.top().second));
@@ -73,14 +76,33 @@ void Drill(std::stack<std::pair<int,int>>&back_track,std::stack<std::pair<int,in
         cur_pos = back_track;
     
 }
-void Finish(std::stack<std::pair<int,int>>&solve_track,std::stack<std::pair<int,int>>&solve_pos, labyrinth &level,Block)
+void Finish(std::stack<std::pair<int,int>>&finish_pos,std::stack<std::pair<int,int>>&solve_pos, labyrinth &level,Block)
 {
 
-    level.maze[solve_track.top().first][solve_track.top().second].display = Block::F;
-    level.maze[solve_track.top().first][solve_track.top().second].visited = true;
+    level.maze[finish_pos.top().first][finish_pos.top().second].display = Block::F;
+    level.maze[finish_pos.top().first][finish_pos.top().second].visited = true;
+    
+    finish_pos.push(std::make_pair(finish_pos.top().first,finish_pos.top().second));
+    
+    finish_pos = finish_pos;
+    
+}
+void Solve(std::stack<std::pair<int,int>>&solve_track,std::stack<std::pair<int,int>>&solve_pos, labyrinth &level,Block)
+{
+ 
+
+    level.maze[solve_pos.top().first][solve_pos.top().second].display = Block::START;
+    level.maze[solve_pos.top().first][solve_pos.top().second].visited = true;
+    if(level.maze[solve_track.top().first][solve_track.top().second].display == Block::START)
+    {
+//       std::cin.get();
+        level.maze[solve_pos.top().first][solve_pos.top().second].display = Block::S;
+
+    }
+
+    
     
     solve_track.push(std::make_pair(solve_track.top().first,solve_track.top().second));
-    
     solve_pos = solve_track;
     
 }
@@ -88,8 +110,8 @@ void Finish(std::stack<std::pair<int,int>>&solve_track,std::stack<std::pair<int,
 
 
 int main() {
-    for(int i = 0; i < 2; i++)
-    {
+    for(int i =0; i < 1000000; i++)
+{
     Block block;
     labyrinth level(SIZE, SIZE);
 //    Bygg maze
@@ -115,84 +137,90 @@ int main() {
     
 
     std::stack<std::pair<int, int>> back_track;
+    std::stack<std::pair<int, int>> finish_pos;
+    std::stack<std::pair<int, int>> finish_track;
     std::stack<std::pair<int, int>> solve_track;
     
     level.maze[1][0].display = block.S;
-    level.maze[1][1].display = block.START;
+
     // sätt S som startplats
+    solve_track.push(std::make_pair(1,1));
     back_track.push(std::make_pair(1,1));
+    finish_track.push(std::make_pair(1,1));
     std::stack<std::pair<int, int>> cur_pos = back_track;
-    std::stack<std::pair<int, int>> solv_pos = solve_track;
+    std::stack<std::pair<int, int>> solve_pos = solve_track;
+
+
     level.maze[back_track.top().first][back_track.top().second].visited = true;  // Sätt Start som besökt
     srand(unsigned(time(NULL)));
-        auto offset = [&](int y, int x)
+        auto chk_neib = [&](int y, int x)
         {
             return std::make_pair(back_track.top().first + y , (back_track.top().second + x));
         };
 
 //    std::cin.get();
     
-    while (!(back_track.empty())) {
-        std::vector<std::pair<int ,int>> neighbors;
+    while (!(back_track.empty()))
+    {
+        std::vector<std::pair<int ,int>> crt_neighbors;
         
 
-        auto north = offset(-1, 0);
+        auto north = chk_neib(-1, 0);
         if(back_track.top().first > 1 && level.maze[north.first][north.second].visited == false)
             if(back_track.top().first > 1 && level.maze[north.first-1][north.second].visited == false && level.maze[north.first][north.second-1].visited == false && level.maze[north.first][north.second+1].visited == false)
-            neighbors.push_back(std::make_pair(north.first,north.second));
+            crt_neighbors.push_back(std::make_pair(north.first,north.second));
         
 
-        auto east = offset(0, 1);
+        auto east = chk_neib(0, 1);
         if(back_track.top().second < level.maze.size()-2 && level.maze[east.first][east.second].visited == false)
                   if(back_track.top().second < level.maze.size()-2 && level.maze[east.first][east.second+1].visited == false && level.maze[east.first-1][east.second].visited == false && level.maze[east.first+1][east.second].visited == false)
-            neighbors.push_back(std::make_pair(east.first,east.second));
+            crt_neighbors.push_back(std::make_pair(east.first,east.second));
             
-        auto west = offset(0,-1);
+        auto west = chk_neib(0,-1);
             if(back_track.top().second > 1 && level.maze[west.first][west.second].visited == false)
-                  if(back_track.top().first > 1 && level.maze[west.first][west.second-1].visited == false && level.maze[west.first+1][west.second].visited == false && level.maze[west.first-1][west.second].visited == false)
-                neighbors.push_back(std::make_pair(west.first,west.second));
+                  if(back_track.top().second > 1 && level.maze[west.first][west.second-1].visited == false && level.maze[west.first+1][west.second].visited == false && level.maze[west.first-1][west.second].visited == false)
+                crt_neighbors.push_back(std::make_pair(west.first,west.second));
         
 
-        auto south = offset(1,0);
+        auto south = chk_neib(1,0);
             if(back_track.top().first < level.maze.size()-2 && level.maze[south.first][south.second].visited == false)
                 if(back_track.top().first < level.maze.size()-2 && level.maze[south.first+1][south.second].visited == false && level.maze[south.first+1][south.second-1].visited == false && level.maze[south.first][south.second+1].visited == false)
-                        neighbors.push_back(std::make_pair(south.first,south.second));
+                        crt_neighbors.push_back(std::make_pair(south.first,south.second));
         
 
-        if(neighbors.empty())
+        if(crt_neighbors.empty())
         {
             
-            //std::cout << " " << std::endl;
-            //std::cout << " Popade " << back_track.size() << std::endl;
-            if(solve_track.size() < back_track.size())
-                solve_track = back_track;
+
+            if(finish_pos.size() < back_track.size())
+                finish_pos = back_track;
             back_track.pop();
             if(back_track.empty()) break;
+            
             if (level.maze[back_track.top().first][back_track.top().second].visited == false)
             {
+                
                 if(back_track.empty()) break;
                 back_track.pop();
                 
+                
+                
             }
-            
-            
         
         }
-        std::random_shuffle(neighbors.begin(), neighbors.end(), myrandom);
+        std::random_shuffle(crt_neighbors.begin(), crt_neighbors.end(), myrandom);
         
         
         
-        for (int i = 0; i  < neighbors.size(); i++) {
-            back_track.push(neighbors.back());
-            neighbors.pop_back();
+        for (int i = 0; i  < crt_neighbors.size(); i++)
+        {
+            back_track.push(crt_neighbors.back());
+            crt_neighbors.pop_back();
             
         }
 
         
         if(back_track.empty()) break;
-        
-        //std::cout << cur_pos.top().first << "," << cur_pos.top().second << std::endl;
-        //std:: cout << back_track.top().first <<"," << back_track.top().second << std::endl;
         
         
         
@@ -201,6 +229,7 @@ int main() {
         if(back_track.top().first > cur_pos.top().first && back_track.top().second == cur_pos.top().second)
         {
             Drill(back_track, cur_pos, level, block);
+            
         
         }
         if(back_track.top().first == cur_pos.top().first && back_track.top().second > cur_pos.top().second)
@@ -219,22 +248,148 @@ int main() {
         
         
     }
-        if (level.maze[solve_track.top().first][solve_track.top().second].visited == true)
+        if (level.maze[finish_pos.top().first][finish_pos.top().second].visited == true)
         {
             
-            Finish(solve_track, cur_pos, level, block);
-            
-            
+            Finish(finish_pos, cur_pos, level, block);
+            level.maze[1][1].display = block.START;
         }
     print(level);
-
     
     
+    
+//    Solve
+    
+    auto solve_neib = [&](int y, int x)
+    {
+        return std::make_pair(solve_track.top().first + y , (solve_track.top().second + x));
+    };
+        while (!(solve_track.empty()))
+        {
+            std::vector<std::pair<int ,int>> solve_neighbors;
+            
+            
+            auto north = solve_neib(-1, 0);
+            if(solve_track.top().first > 0 && level.maze[north.first][north.second].display == Block::PATH)
+                    solve_neighbors.push_back(std::make_pair(north.first ,north.second));
+            if(solve_track.top().first > 0 && level.maze[north.first][north.second].display == Block::F)
+            {
+                level.maze[north.first+1][north.second].display = Block::START;
+                break;
+            }
+            
+            auto east = solve_neib(0, 1);
+            if(solve_track.top().second < level.maze.size() && level.maze[east.first][east.second].display == Block::PATH)
+                    solve_neighbors.push_back(std::make_pair(east.first,east.second));
+            if(solve_track.top().second < level.maze.size() && level.maze[east.first][east.second].display == Block::F)
+            {
+                level.maze[east.first][east.second-1].display = Block::START;
+                break;
+            }
+            
+            std::cout << solve_track.top().first << solve_track.top().second << std::endl;
+            auto west = solve_neib(0,-1);
+            
+            if(solve_track.top().second > 1 && level.maze[west.first][west.second].display == Block::PATH)
+                    
+                    solve_neighbors.push_back(std::make_pair(west.first,west.second));
+                    
+            if(solve_track.top().second > 0 && level.maze[west.first][west.second].display == Block::F)
+            {
+                level.maze[west.first][west.second+1].display = Block::START;
+                break;
+            }
+            
+            
+            auto south = solve_neib(1,0);
+            
+            if(solve_track.top().first < level.maze.size()-2 && level.maze[south.first][south.second].display == Block::PATH)
+                    solve_neighbors.push_back(std::make_pair(south.first,south.second));
+            if(solve_track.top().first < level.maze.size() && level.maze[south.first][south.second].display == Block::F)
+            {
+                level.maze[south.first-1][south.second].display = Block::START;
+                break;
+            }
+            
+            //std::cout << std::endl;
+            std::cout << solve_neighbors.size() << std::endl;
+            
 
+
+            if(solve_neighbors.empty())
+            {
+                
+
+                if (level.maze[solve_pos.top().first][solve_pos.top().second].visited == true)
+                {
+                    solve_track.pop();
+                    solve_pos.pop();
+                    if(solve_track.empty())
+                        break;
+
+                    
+                }
+                
+                solve_track.pop();
+                
+            }
+            
+            
+            for (int i = 0; i  < solve_neighbors.size(); i++)
+            {
+                solve_track.push(solve_neighbors.back());
+                solve_neighbors.pop_back();
+                
+            }
+            if(solve_track.top().first == solve_pos.top().first)
+            
+
+            
+            
+            if(solve_track.empty()) break;
+            
+            if(solve_track.top().first > solve_pos.top().first && solve_track.top().second == solve_pos.top().second)
+            {
+                Solve(solve_track, solve_pos, level, block);
+
+            }
+            
+
+            if(solve_track.top().first == solve_pos.top().first && solve_track.top().second > solve_pos.top().second)
+            {
+
+                Solve(solve_track, solve_pos, level, block);
+
+            }
+            
+            if(solve_track.top().first < solve_pos.top().first && solve_track.top().second == solve_pos.top().second)
+            {
+
+                Solve(solve_track, solve_pos, level, block);
+
+            }
+            if(solve_track.top().first == solve_pos.top().first && solve_track.top().second < solve_pos.top().second)
+            {
+
+                Solve(solve_track, solve_pos, level, block);
+
+            }
+            
+            print(level);
+            
+        }
+    
 
     
-    
+    std::cout << std::endl;
+    std::cout << i << std::endl;
+    std::cout << i << std::endl;
+    std::cout << i << std::endl;
 
-    }
+
+     print(level);
+    std::cin.get();
+}
+    
      return 0;
 }
